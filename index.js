e72e99c8f9955c11dac0a10022d3d01d37aa46ec
Mariel Segovia <msegovia94@gmail.com>
2018-04-23 02:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(name) {
 
 function removeLastKitten() {
   //removes last kitten and returns a new array// array unchanged
-  var newKittens = kittens.slice(0, 2)
+  var newKittens = kittens.slice(0, kittens.length - 1)
   return newKittens
 }
